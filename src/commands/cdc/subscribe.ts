e882ea508e3ea282b8417ff9094fdb537ad4d2f6
pogilvie <code@ogilvie.us.com>
2021-03-14 07:35:10 -0700

    polish

---
+++ src/commands/cdc/subscribe.ts
@@ -0,0 +1,44 @@
+import { SfdxCommand } from '@salesforce/command';
+import { AnyJson } from '@salesforce/ts-types';
+import { CometD } from 'cometd';
+import { adapt } from 'cometd-nodejs-client';
+
+export default class Subscribe extends SfdxCommand {
+  
+  protected static requiresUsername = true;
+  protected static requiresProject = false;
+  public static description = "Subscribe to an org's change data capture stream"
+  public static examples = [
+    `$ sfdx cdc:subscribe  -u my-org-alias
+    `
+  ]
+  
+  public async run(): Promise<AnyJson> {
+    
+    const c = this.org.getConnection()
+    
+    adapt()
+
+    const cometd = new CometD();
+
+    cometd.configure({
+      url: c.instanceUrl + '/cometd/44.0',
+      requestHeaders: {
+        Authorization: 'Bearer ' + c.accessToken
+      },
+      appendMessageTypeToURL: false
+    })
+
+    cometd.handshake( handshake => {
+      if (handshake.successful) {
+        console.log('ComedD handshared successful')
+        cometd.subscribe('/data/ChangeEvents', msg => {
+          console.log(JSON.stringify(msg, null, 4))
+        })
+      }
+    })
+  
+    return <AnyJson><unknown>null
+
+  }
+}
