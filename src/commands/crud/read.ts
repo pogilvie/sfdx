0316593a7e527169732b3a3d89306f7676788326
Peter Ogilvie <peter.ogilvie@coinbase.com>
2021-03-21 07:15:48 -0700

    crud:delete, crud:read

--- src/commands/crud/read.ts
+++ src/commands/crud/read.ts
@@ -1,14 +1,14 @@
 import { flags, SfdxCommand } from '@salesforce/command';
 import { AnyJson } from '@salesforce/ts-types';
 
-export default class Delete extends SfdxCommand {
+export default class Read extends SfdxCommand {
 
   protected static requiresUsername = true;
   protected static requiresProject = false;
-  public static description = 'Delete a record using REST using a recordId'
+  public static description = 'Read a record using REST using a recordId'
   public static examples = [
-    `$ sfdx crud:delete -s Account, -i 00102000007GstXAAS  -u my-org-alias
-    $ sfdx crud:delete -s Account, -i 00102000007GstXAAS,00102000007GsveAAC -u my-org-alias
+    `$ sfdx crud:read -s Account, -i 00102000007GstXAAS  -u my-org-alias
+    $ sfdx crud:read -s Account, -i 00102000007GstXAAS,00102000007GsveAAC -u my-org-alias
     `
   ]
 
@@ -31,18 +31,15 @@ export default class Delete extends SfdxCommand {
     const ids = this.flags.ids.split(',')
 
     const c = this.org.getConnection();
-    c.sobject(this.flags.sobject).delete(ids)
-    .then( result => {
-      if (result.success) {
-        console.log(result)
-      } else {
-        console.log(JSON.stringify(result))
+    const result = await c.sobject(this.flags.sobject).retrieve(ids) as Array<object>
+
+    result.forEach( r => {
+      for (let field in r) {
+        if (r[field] && field != 'attributes') {
+          console.log(`${field}: ${r[field]}`)
+        }
       }
     })
-    .catch( error => {
-      console.log('Delete call Failed', JSON.stringify(error))
-    }) 
-
-    return <AnyJson><unknown>null
+    return <AnyJson><unknown>result
   }
 }
